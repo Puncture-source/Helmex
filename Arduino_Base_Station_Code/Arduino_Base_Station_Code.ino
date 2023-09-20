e680116da1e64f1b3ffb05fae8654eeefbdbd167
Puncture-source <74575565+Puncture-source@users.noreply.github.com>
2023-09-20 19:51:04 +1000

    Update Arduino_Base_Station_Code.ino
    Added output smoothing, finding average over ten outputs and seeing avg

--- Arduino_Base_Station_Code/Arduino_Base_Station_Code.ino
+++ Arduino_Base_Station_Code/Arduino_Base_Station_Code.ino
@@ -10,8 +10,26 @@ const char* deviceServiceCharacteristicUuid2 = "22222222-aaaa-bbbb-cccc-01234567
 
 byte temp;
 
+//------------[Sensor Smoothing]-----------------
+// Define the number of samples to keep track of. The higher the number, the
+// more the readings will be smoothed, but the slower the output will respond to
+// the input. Using a constant rather than a normal variable lets us use this
+// value to determine the size of the readings array.
+const int numReadings = 10;
+
+int readings[numReadings];  // the readings from the analog input
+int readIndex = 0;          // the index of the current reading
+int total = 0;              // the running total
+int average = 0;            // the average
+
+//-----------------------------------------------
+
 void setup() {
   Serial.begin(9600);
+  for (int thisReading = 0; thisReading < numReadings; thisReading++) {
+  readings[thisReading] = 0;
+  }
+
   while (!Serial);
 
   pinMode(LED_BUILTIN, OUTPUT);
@@ -148,6 +166,31 @@ void controlPeripheral(BLEDevice peripheral) {
     }
     */
     
+    
+    //-------------[Smoothing]----------
+    // subtract the last reading:
+  total = total - readings[readIndex];
+  // read from the sensor:
+  readings[readIndex] = peripheral.rssi();
+  // add the reading to the total:
+  total = total + readings[readIndex];
+  // advance to the next position in the array:
+  readIndex = readIndex + 1;
+
+  // if we're at the end of the array...
+  if (readIndex >= numReadings) {
+    // ...wrap around to the beginning:
+    readIndex = 0;
+  }
+
+  // calculate the average:
+  average = total / numReadings;
+  // send it to the computer as ASCII digits
+  Serial.println(average);
+  delay(1);  // delay in between reads for stability
+  //---------------------------------------------------------
+
+
   }while(1!=1);
   
   }
